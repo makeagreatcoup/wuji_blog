b082c291c5d78e8e8ebd0754ba12047f5876921e
王晶 <1015861859@qq.com>
2021-08-14 15:09:16 +0800

    初始化

---
+++ src/main/java/com/wuji/wangj/WangjApplication.java
@@ -0,0 +1,13 @@
+package com.wuji.wangj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WangjApplication.class, args);
+	}
+
+}
